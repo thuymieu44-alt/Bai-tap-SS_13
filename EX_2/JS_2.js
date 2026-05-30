8fb9ca21833a0f22f3c5cbc2bafaab1eadf39ca1
Sam Thi Thuy <thuymieu44@gmail.com>
2026-05-30 21:35:26 +0900

    Add files via upload

---
+++ EX_2/JS_2.js
@@ -0,0 +1,7 @@
+let input = Number(prompt('Mời nhập số bất kì:'));
+let numbers = ['4', '8', '13', '25', '40'];
+if (numbers.includes(String(input))) {
+    alert('Bongooo!');
+} else {
+    alert('Chúc bạn may mắn lần sau.');
+} 
